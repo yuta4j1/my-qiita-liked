8139e155f2275fdc0f76f4973f1739ce7e718bab
yuta4j1 <kascado.ys10@gmail.com>
2019-05-05 23:37:26 +0900

    fix: get number of all-page (remove hard coding)

--- src/main/java/com/yuta4j1/myqiitaliked/controller/MyLikedArticleController.java
+++ src/main/java/com/yuta4j1/myqiitaliked/controller/MyLikedArticleController.java
@@ -2,27 +2,34 @@ package com.yuta4j1.myqiitaliked.controller;
 
 import com.yuta4j1.myqiitaliked.crawler.QiitaCrawler;
 import com.yuta4j1.myqiitaliked.model.ArticleJson;
-import com.yuta4j1.myqiitaliked.model.PageParameter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Qiitaのいいね記事一覧情報エンドポイント
+ */
 @RestController
 @RequestMapping("/mql")
 public class MyLikedArticleController {
 
+    /**
+     * Qiitaのいいね一覧情報を取得する
+     *
+     * @return
+     */
     @RequestMapping(method = RequestMethod.GET, value = "/list")
     public List<ArticleJson> fetchLikedArticleInfo() {
-        int pageNum = 12;
         String baseUrl = "https://qiita.com/ggggoooo/like";
+        QiitaCrawler firstCrawler = new QiitaCrawler(baseUrl);
+        int pageNum = firstCrawler.getAllPageNum();
         List<ArticleJson> resultList = new ArrayList<>();
         for (int i = 1; i <= pageNum; i++) {
             if (i == 1) {
-                resultList.addAll(new QiitaCrawler(baseUrl).fetchArticleList());
+                resultList.addAll(firstCrawler.fetchArticleList());
             } else {
                 resultList.addAll(new QiitaCrawler(baseUrl + "?page=" + String.valueOf(i)).fetchArticleList());
             }
